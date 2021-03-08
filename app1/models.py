d608758069f72146e7c5f9799fa4e974ecab1d4d
fedorovra <fedorov.r.a@hotmail.com>
2021-03-08 14:51:03 +0300

    creating model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class APIKeys(models.Model):
+    api_key = models.CharField(max_length=25, unique=True)
+    permissions = models.CharField(max_length=250, blank=True, null=True)
+    is_active = models.CharField(max_length=1, default='Y', blank=False, null=False)
+
